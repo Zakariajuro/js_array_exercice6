8ead4edbe8a501e490e061e5b607d3e769cabcdf
Zakaria <zakPro@live.be>
2021-01-04 11:14:39 +0100

    exo-fini-manque-quelque-point

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+// EXERCICE 1
+
+// let monTab = ['pomme', 'fraise', 'melon', 'citron'];
+// monTab.pop();
+// monTab.shift();
+// monTab.push('Orange');
+// monTab.unshift('Strawberry');
+// // monTab = (monTab[0].toLowerCase() + monTab[1].toUpperCase() + monTab.slice[2]);
+// monTab.reverse();
+// console.log(monTab);
+
+// EXERCICE 2
+
+// let chiffres = [4, 5, 2, 1, 3];
+// chiffres.sort();
+
+// console.log(chiffres);
+
+// EXERCICE 3
+
+let tab = ['pomme', 'fraise', 'melon', 'citron','Orange','Strawberry','voiture', 'velo', 'moto', 'bateaux','avion']
+console.log(tab);
+
+console.log(tab[3]);   
+
+let pos = 1, n = 2;
+tab.splice(pos, n);
+
+console.log(tab.length);
